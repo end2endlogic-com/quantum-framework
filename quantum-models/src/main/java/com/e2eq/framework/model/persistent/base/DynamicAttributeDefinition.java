188bb2e2b46c4720f829d190ea380b7ac15693a8
Michael Ingardia <mingardia@end2endlogic.com>
2025-09-05 13:23:23 -0400

    add no arg constructor to DynamicAttributeDefinition

--- quantum-models/src/main/java/com/e2eq/framework/model/persistent/base/DynamicAttributeDefinition.java
+++ quantum-models/src/main/java/com/e2eq/framework/model/persistent/base/DynamicAttributeDefinition.java
@@ -3,10 +3,7 @@ package com.e2eq.framework.model.persistent.base;
 import dev.morphia.annotations.Entity;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NonNull;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import java.util.List;
@@ -15,6 +12,7 @@ import java.util.List;
 @SuperBuilder
 @EqualsAndHashCode
 @RegisterForReflection
+@NoArgsConstructor
 @Entity
 public class DynamicAttributeDefinition {
     @NonNull
